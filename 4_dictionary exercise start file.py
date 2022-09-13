15e5f9872d18d1d3d05efb68ca04f78ac2d81461
Blake <blake_gray1@baylor.edu>
2022-09-13 12:14:46 -0500

    Classwork 9/13/22

--- 4_dictionary exercise start file.py
+++ 4_dictionary exercise start file.py
@@ -1,8 +1,9 @@
+import random
 # This program uses a dictionary as a deck of cards.
 
 def main():
     # Create a deck of cards.
-   
+    deck = create_deck()
 
     # Get the number of cards to deal.
     num_cards = int(input('How many cards should I deal? '))
@@ -11,7 +12,7 @@ def main():
 
     # Deal the cards.
 
-
+    deal_cards(deck,num_cards)
     
     
 
@@ -46,7 +47,7 @@ def create_deck():
 
     # Return the deck.
 
-
+    return deck
 
 
 # The deal_cards function deals a specified number of cards
@@ -55,24 +56,28 @@ def create_deck():
 def deal_cards(deck, number):
     # Initialize an accumulator for the hand value.
 
-    
+    handValue = 0
     
 
     # Make sure the number of cards to deal is not
     # greater than the number of cards in the deck.
-
+    if number > 52:
+        number = 52
     
     
 
     # Deal the cards and accumulate their values.
-    
-
+    for count in range(number):
+        card = random.choice(list(deck))
+        value = deck[card]
+        handValue += value
+        print(card)
 
     
 
     # Display the value of the hand.
 
-    
+    print("The value of the hand is: ",handValue)
     
 
 # Call the main function.
